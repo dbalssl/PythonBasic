f182ffb55ac3ef98096492ba2a3b4912ba400f85
yumin <yumin8091@naver.com>
2024-10-31 17:24:44 +0900

    Add files via upload

--- webcrawling/ch02_daum_news_one.py
+++ webcrawling/ch02_daum_news_one.py
@@ -30,9 +30,27 @@ contents = doc.select("section > p")  # [<p>본1</p>, <p>본2</p>]
 content = ""
 for text in contents:
     # text → <p>본1</p>
-    text.get_text() # → "본1 + 본2"
+    content += text.get_text() # → "본1"
 
-print("=" * 50)
+writer = doc.select("span.txt_info")[0].get_text()
+
+
+reg_date = doc.select("span.num_date")[0].get_text()
+# 2024. 10. 28. 16:40
+list_date = reg_date.split(".")
+
+# 1. 단독 strip()
+# reg_date = list_date[0] + list_date[1].strip() + list_date[2].strip()
+# 2. 리스트 컴프리핸션
+# ["년", "월", "일", "시간"]
+# list_date = [x.strip() for x in list_date]
+
+# 3. lambda식
+#   - map, reduce, filter
+list_date = list(map(lambda x: x.strip(), list_date))
+reg_date = list_date[0] + list_date[1] + list_date[2]
+
 print(f"뉴스 제목 : {title}")
-print("=" * 50)
-print(f"뉴스 본문: {content}")
+print(f"뉴스 기자 : {writer}")
+print(f"뉴스 날짜 : {reg_date}") 
+print(f"뉴스 본문 : {content}")
