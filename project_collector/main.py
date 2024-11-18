cd6b0c02b4c8945056cc95a88655be5317aa7864
yumin <yumin8091@naver.com>
2024-11-18 15:18:53 +0900

    Add files via upload

--- project_collector/main.py
+++ project_collector/main.py
@@ -1,5 +1,15 @@
 # 데이터를 수집할 수 있는 웹사이트 개발!
 #  1. 화면: 특정 정보 입력(streamlit)
-#  2. 수집: 데이터 수집(requests, beautifulsoup)
+#  2. 수집: 데이터 수집(requests, beautifulSoup)
 #  3. 화면: 출력, 엑셀 다운로드(streamlit, pandas)
 #  4. 저장: 데이터베이스 저장(pymysql + MariaDB)
+from fnc_service import collect_news
+
+category = "digital" #IT
+
+def main():
+    print("Start collector")
+    collect_news(category)
+    
+if __name__ == "__main__":
+    main()
