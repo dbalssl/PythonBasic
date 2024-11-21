71f4d7a62e0ed094f692238e67865e6f1d548a8f
yumin <yumin8091@naver.com>
2024-11-21 17:08:08 +0900

    Add files via upload

--- project_collector/main.py
+++ project_collector/main.py
@@ -10,7 +10,6 @@ import streamlit as st
 # Streamlit docs → https://docs.streamlit.io/
 # Emoji → https://snskeyboard.com/emoji/
 
-category = "digital" #IT
 
 def main():
     st.set_page_config(
@@ -20,32 +19,17 @@ def main():
     st.title("NEWS :red[COLLECTOR]")
     st.text("실시간 뉴스를 수집합니다.")
     
-    category = {
-        "사회": "society",
-        "정치": "politics",
-        "경제": "economic",
-        "국제": "foreign",
-        "문화": "culture",
-        "IT": "digital"
-    }
-    with st.expander(label="뉴스 카테고리", expanded=False):
-        for key, value in category.items():
-            st.text(f"{key}({value})")
-    
     with st.form(key="form"):
         # 유효성 체크 → 사용자가 입력한 값이 유효한 값인지 체크
         #                ㄴ 전처리(가공) → 유효한 값으로 변경
         # 1. 정규식을 활용한 유효성 체크
         # 2. 엑셀 파일로 생성 다운로드
         # 3. github 정리 + README.md(꾸미기)
-        sel_category = st.text_input(label="수집하고 싶은 뉴스 카테고리")
-        st.write(sel_category)
-        st.form_submit_button("수집")
+        submitted = st.form_submit_button("수집")
     
-    
-    
-   # print("Start collector")
-   # collect_news(category)
+    # 버튼 이벤트 → 수집버튼을 클릭했을 때
+    if submitted:
+        collect_news()
     
 if __name__ == "__main__":
     main()
