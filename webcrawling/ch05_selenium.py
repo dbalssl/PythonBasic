411b7ccd73047cbbe7d71d0db97284a5897e3839
yumin <yumin8091@naver.com>
2024-11-25 16:44:42 +0900

    Add files via upload

---
+++ webcrawling/ch05_selenium.py
@@ -0,0 +1,46 @@
+# SELENIUM
+#  - 설치: pip install selenium
+#  - 웹 크롤링(정적, 동적 모두 가능) + 자동화
+#      ㄴ SELENIUM이 제어할 수 있는 웹 브라우저 사용
+#  - 과거에 웹브라우저 테스트 도구!
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+# 디폴트: SELENIUM 코드 종료 → 웹브라우저 종료
+# 1. 셀레니움 웹브라우저 설정
+options = Options()
+options.add_experimental_option("detach",True) # 웹브라우저 종료 X
+# options.add_argument("headless") # 셀레니움 백그라운드 동작(개발 완료)
+
+# ※ 셀레니움이 로봇이 아닌척 하는 방법
+options.add_argument("disable-blink-features=AutomationControlled")
+options.add_experimental_option("useAutomationExtension", False)
+options.add_experimental_option("excludeSwitches",["enable-automation"])
+
+
+
+# ※ 셀레니움 4버전 이하에서는 ChromeDriver를 설치해서 사용!
+#    4버전부터는 설치할 필요 없음!
+#    service = Service(ChromeDriverManaer().install())
+
+# 2. 셀레니움 웹브라우저 생성
+driver = webdriver.Chrome(options=options)
+
+# 3. 셀레니움 웹브라우저 사용
+driver.get("http://www.naver.com")
+time.sleep(1)
+print(driver.page_source) # → 네이버 메인페이지 소스코드
+
+search = driver.find_element(By.ID, "query")
+search.send_keys("정우성")
+search.send_keys(Keys.ENTER)
+time.sleep(1)
+
+# 현재 웹브라우저 페이지의 소스코드 GET
+print(driver.page_source) # → 정우성 검색 네이버 페이지 소스코드
+
+# + BeautifulSoup
