6f54ce188cc2cdf85e9d217424fef5d233a39353
yumin <yumin8091@naver.com>
2024-11-28 17:25:23 +0900

    Add files via upload

---
+++ project_lms/main.py
@@ -0,0 +1,12 @@
+# 도서관 관리 프로그램
+#  - 도서만 관리 프로그램
+#  - 기능
+#    1) 도서 등록  Create
+#    2) 도서 수정  Update
+#    3) 도서 삭제  Delete
+#    4) 도서 조회  Read
+#    5) 도서 검색  Read
+#  - CRUD 개발 학습 → 모든 시스템 또는 개발 프로그램 CRUD 개발
+#  - 프론트엔드: Streamlit
+#  - 백엔드: Python + SQL
+#  - DB: Mariadb
